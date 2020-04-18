49d2a7c5995977eac42a2d977e9e16b1263bb776
krojerb <krojerb@cip.ifi.lmu.de>
2020-04-18 16:15:13 +0200

    wip

--- scripts/templates/eval.py
+++ scripts/templates/eval.py
@@ -1,12 +1,27 @@
+import json
+
 best = float('inf')
 genders = ['er', 'sie', 'es']
+predictions = {'er': [], 'sie': [], 'es': []}
+best_en = ''
+best_de = ''
 for i, (score, de, en) in enumerate(zip(open('../../outputs/templates/concat22', 'r'), open('../../templates/de_tok',
     'r').readlines(), open('../../templates/en_tok', 'r').readlines())):
+    score = float(score)
     if best > score:
         best = score
-        gender = genders[i%3]
+        gender = genders[i % 3]
+        best_de = de
+        best_en = en
     if i % 3 == 2:
+        predictions[gender].append(best_de.strip() + ' - ' + best_en.strip())
+        best = float('inf')
+        best_de = ''
+        best_en = ''
 
+for key, val in predictions.items():
+    print(f'{key}: {len(val)}')
+json.dump(predictions, open('../../outputs/templates/results.json', 'w'))
 
 
 
