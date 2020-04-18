6d63b5bfe26b4ed3d93b79e6ac4c9bcb3b28fb11
krojerb <krojerb@cip.ifi.lmu.de>
2020-04-19 00:25:41 +0200

    wip

--- scripts/templates/dist_create_template.py
+++ scripts/templates/dist_create_template.py
@@ -15,22 +15,24 @@ pairs = []
 
 for m_en,m_de in nouns['m']:
     for f_en,f_de in nouns['f']:
-        pairs.append({('m', m_en, m_de), ('f', f_en, f_de)})
+        pairs.append((('m', m_en, m_de), ('f', f_en, f_de)))
     for f_en,f_de in nouns['n']:
-        pairs.append({('m', m_en, m_de), ('n', f_en, f_de)})
+        pairs.append((('m', m_en, m_de), ('n', f_en, f_de)))
 
 for m_en,m_de in nouns['f']:
     for f_en,f_de in nouns['n']:
-        pairs.append({('f', m_en, m_de), ('n', f_en, f_de)})
+        pairs.append((('f', m_en, m_de), ('n', f_en, f_de)))
 
-with open('../../templates/distance/de_tok', 'w') as tokenized_de, open('../../templates/distance/en_tok', 'w') \
+with open('../../templates/distance/and_de_tok', 'w') as tokenized_de, open('../../templates/distance/and_en_tok',
+                                                                            'w') \
         as tokenized_en, MosesPunctuationNormalizer('en') as norm, MosesTokenizer('de') as tok_de, MosesTokenizer(
-    'en') as tok_en, open('../../templates/distance/gender_combination','w') as gender_file:
+    'en') as tok_en, open('../../templates/distance/gender_combination', 'w') as gender_file:
     for first, second in pairs:
-        en_template1 = ' '.join(tok_en(norm(f'The {first[1]} is next to the {second[1]}. I look at it.')))
+        en_template1 = ' '.join(tok_en(norm(f'I am now standing in front of the {first[1]} and the {second[1]}. I '
+                                            f'carefully observe it.')))
 
-        en_template2 = ' '.join(tok_en(norm(f'The {second[1]} is next to the {first[1]}. I look at it.')))
-
+        en_template2 = ' '.join(tok_en(norm(f' am now standing in front of the {second[1]} and the {first[1]}. I '
+                                            f'carefully look at.')))
         for _ in range(3):
             tokenized_en.write(en_template1 + '\n')
             gender_file.write(first[0]+second[0]+'\n')
@@ -38,38 +40,42 @@ with open('../../templates/distance/de_tok', 'w') as tokenized_de, open('../../t
             tokenized_en.write(en_template2 + '\n')
             gender_file.write(second[0] + first[0] + '\n')
 
-        ihn1 = ' '.join(tok_de(norm(f'{nominative[first[0]]} {first[2]} ist neben {dativ[second[0]]} {second[2]}. '
-                                    f'Ich schaue ihn an.')))
+        ihn1 = ' '.join(tok_de(norm(f'Ich stehe nun vor {dativ[first[0]]} {first[2]} und {dativ[second[0]]}'
+                                    f' {second[2]}. '
+                                    f'Ich beobachte ihn sorgfältig.')))
 
         tokenized_de.write(ihn1+'\n')
-        sie1 = ' '.join(tok_de(norm(f'{nominative[first[0]]} {first[2]} ist neben {dativ[second[0]]} {second[2]}. '
-                                    f'Ich schaue sie an.')))
+        sie1 = ' '.join(tok_de(norm(f'Ich stehe nun vor {dativ[first[0]]} {first[2]} und {dativ[second[0]]}'
+                                    f' {second[2]}. '
+                                    f'Ich beobachte sie sorgfältig.')))
 
         tokenized_de.write(sie1+'\n')
-        es1 = ' '.join(tok_de(norm(f'{nominative[first[0]]} {first[2]} ist neben {dativ[second[0]]} {second[2]}. '
-                                    f'Ich schaue es an.')))
+        es1 = ' '.join(tok_de(norm(f'Ich stehe nun vor {dativ[first[0]]} {first[2]} und {dativ[second[0]]}'
+                                    f' {second[2]}. '
+                                    f'Ich beobachte es sorgfältig.')))
 
         tokenized_de.write(es1+'\n')
 
-        ihn2 = ' '.join(tok_de(norm(f'{nominative[second[0]]} {second[2]} ist neben {dativ[first[0]]} {first[2]}. '
-                                    f'Ich schaue ihn an.')))
-
+        ihn2 = ' '.join(tok_de(norm(f'Ich stehe nun vor {dativ[second[0]]} {second[2]} und {dativ[first[0]]}'
+                                    f' {first[2]}. '
+                                    f'Ich beobachte ihn sorgfältig.')))
         tokenized_de.write(ihn2+'\n')
-        sie2 = ' '.join(tok_de(norm(f'{nominative[second[0]]} {second[2]} ist neben {dativ[first[0]]} {first[2]}. '
-                                    f'Ich schaue sie an.')))
+        sie2 = ' '.join(tok_de(norm(f'Ich stehe nun vor {dativ[second[0]]} {second[2]} und {dativ[first[0]]}'
+                                    f' {first[2]}. '
+                                    f'Ich beobachte sie sorgfältig.')))
 
         tokenized_de.write(sie2+'\n')
 
-        es2 = ' '.join(tok_de(norm(f'{nominative[second[0]]} {second[2]} ist neben {dativ[first[0]]} {first[2]}. '
-                                   f'Ich schaue es an.')))
-
+        es2 = ' '.join(tok_de(norm(f'Ich stehe nun vor {nominative[second[0]]} {second[2]} und {dativ[first[0]]}'
+                                    f' {first[2]}. '
+                                    f'Ich beobachte es sorgfältig.')))
         tokenized_de.write(es2+'\n')
 
 
 command = 'subword-nmt apply-bpe -c ../../models_dario/subtitles/ende.bpe --glossaries "<SEP>" < ' \
-          '../../templates/distance/en_tok > ../../templates/distance/en_bpe'
+          '../../templates/distance/and_en_tok > ../../templates/distance/and_en_bpe'
 os.system(command)
 
 command = 'subword-nmt apply-bpe -c ../../models_dario/subtitles/ende.bpe --glossaries "<SEP>" < ' \
-                  '../../templates/distance/de_tok > ../../templates/distance/de_bpe'
+                  '../../templates/distance/and_de_tok > ../../templates/distance/and_de_bpe'
 os.system(command)
--- scripts/templates/eval_dist.py
+++ scripts/templates/eval_dist.py
@@ -1,22 +1,33 @@
 import json
 
-predictions = {'same_position': [], 'same_antecedent': []}
+gender_order = ['m','f','n']
+predictions = {'first_pos': [], 'second_pos': [], 'same_antecedent': [], 'other': []}
 acc_scores = []
 acc_en = []
 acc_de = []
+genders = open('../../templates/distance/gender_combination', 'r').readlines()
 for i, (score, de, en) in enumerate(zip(open('../../outputs/templates/concat22_and_dist', 'r'),
                                         open('../../templates/distance/and_de_tok', 'r').readlines(),
                                         open('../../templates/distance/and_en_tok', 'r').readlines())):
 
     acc_scores.append(float(score)), acc_en.append(en), acc_de.append(de)
     if i % 6 == 5:
+        first_gender = genders[i-5][0]
+        second_gender = genders[i][0]
         bestidx1 = acc_scores[:3].index(min(acc_scores[:3]))
         bestidx2 = acc_scores[3:].index(min(acc_scores[3:]))
+
         if bestidx1 == bestidx2:
             predictions['same_antecedent'].append(
                 acc_de[bestidx1].strip() + '///' + acc_de[3:][bestidx2].strip())
+        elif bestidx1 == gender_order.index(first_gender) and bestidx2 == gender_order.index(second_gender):
+            predictions['first_pos'].append(
+                acc_de[bestidx1].strip() + '///' + acc_de[3:][bestidx2].strip())
+        elif bestidx1 == gender_order.index(second_gender) and bestidx2 == gender_order.index(first_gender):
+            predictions['second_pos'].append(
+                acc_de[bestidx1].strip() + '///' + acc_de[3:][bestidx2].strip())
         else:
-            predictions['same_position'].append(
+            predictions['other'].append(
                 acc_de[bestidx1].strip() + '///' + acc_de[3:][bestidx2].strip())
         acc_scores, acc_en, acc_de = [], [], []
 
