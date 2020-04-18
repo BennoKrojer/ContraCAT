c541160910f0c5c75ee5df86fe587f1a738a9ddb
krojerb <krojerb@cip.ifi.lmu.de>
2020-04-18 12:02:15 +0200

    true modification

--- scripts/modify_context.py
+++ scripts/modify_context.py
@@ -29,19 +29,19 @@ def append(line, phrase, new_sentence=False):
     return context, sent
 
 
-de_modification = 'punkt'
-en_modification = 'point'
+de_modification = 'that_true'
+en_modification = 'that_true'
 de_path = '../ContraPro_Dario/contrapro.text.tok.prev.de.de'
 en_path = '../ContraPro_Dario/contrapro.text.tok.prev.en.en'
-output_de = f'../ContraPro_Dario/modified/{de_modification}_de_tok.txt'
-output_en = f'../ContraPro_Dario/modified/{en_modification}_en_tok.txt'
+output_de = f'../ContraPro_Dario/subtitle_bpe/modified/{de_modification}_de_tok.txt'
+output_en = f'../ContraPro_Dario/subtitle_bpe/modified/{en_modification}_en_tok.txt'
 
 with MosesPunctuationNormalizer('de') as norm, MosesTokenizer('de') as tok, MosesDetokenizer('de') as de_tok:
     with open(de_path, 'r') as de_file, open(output_de, 'w') as out:
         for _, line in tqdm(enumerate(de_file)):
             # print(line)
             line = de_tok(line.split())
-            context, sent = append(line, ', aber das ist nicht der punkt')
+            context, sent = append(line, ' und das ist wahr')
             context, sent = norm(context), norm(sent)
             if context:
                 if de_modification == "er_sagte" and context[-1] in string.punctuation:
@@ -56,7 +56,7 @@ with MosesPunctuationNormalizer('en') as norm, MosesTokenizer('en') as tok, Mose
     with open(en_path, 'r') as en_file, open(output_en, 'w') as out:
         for _, line in tqdm(enumerate(en_file)):
             line = de_tok(line.split())
-            context, sent = append(line, " but that's not the point")
+            context, sent = append(line, " and that is true")
             context, sent = norm(context), norm(sent)
             if context:
                 if de_modification == "er_sagte" and context[-1] in string.punctuation:
@@ -67,18 +67,9 @@ with MosesPunctuationNormalizer('en') as norm, MosesTokenizer('en') as tok, Mose
                 sent = '<SEP> ' + ' '.join(tok(sent)) + '\n'
                 out.write(sent)
 
-command_de = f'subword-nmt apply-bpe -c ../ted_data/train/ende.bpe --glossaries "<SEP>" < ../ContraPro_Dario/modified/{de_modification}_de_tok.txt > tmp_de.txt'
-command_en = f'subword-nmt apply-bpe -c ../ted_data/train/ende.bpe --glossaries "<SEP>" < ../ContraPro_Dario/modified/{en_modification}_en_tok.txt > tmp_en.txt'
+command_de = f'subword-nmt apply-bpe -c ../models_dario/subtitles/ende.bpe --glossaries "<SEP>" < ' \
+             f'{output_de} > {output_de.replace("tok", "bpe")}'
+command_en = f'subword-nmt apply-bpe -c ../ted_data/train/ende.bpe --glossaries "<SEP>" < {output_en} > ' \
+             f'{output_en.replace("tok", "bpe")}'
 os.system(command_de)
 os.system(command_en)
-
-with open('tmp_de.txt', 'r') as tmp_de, open(f'../ContraPro_Dario/modified/{de_modification}_de_bpe.txt', 'w') as bpe_de:
-    for line in tmp_de:
-        bpe_de.write(line)
-
-with open('tmp_en.txt', 'r') as tmp_en, open(f'../ContraPro_Dario/modified/{en_modification}_en_bpe.txt', 'w') as bpe_en:
-    for line in tmp_en:
-        bpe_en.write(line)
-
-os.system('rm -rf tmp_de.txt')
-os.system('rm -rf tmp_en.txt')
