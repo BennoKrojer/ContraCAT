d3823b8bc902319dd30bac5a4d8111c5e8d2005b
Benno Krojer <krojerb@cis.uni-muenchen.de>
2020-04-18 17:01:34 +0200

    scored more true

--- scripts/score.sh
+++ scripts/score.sh
@@ -1,1 +1,2 @@
-python3 -m sockeye.score --target ContraPro_Dario/subtitle_bpe/modified/true/true:_de_bpe.txt --source ContraPro_Dario/subtitle_bpe/modified/true/true:_en_bpe.txt --output outputs/subtitles/true/true: --model models_dario/subtitles/concat-2-2/ --device-ids 0 --output-type score --batch-size 128
+python3 -m sockeye.score --target ContraPro_Dario/subtitle_bpe/modified/true/true:_no_quot_de_bpe.txt --source ContraPro_Dario/subtitle_bpe/modified/true/true:_no_quot_en_bpe.txt --output outputs/subtitles/true/true:_no_quot --model models_dario/subtitles/concat-2-2/ --device-ids 0 --output-type score --batch-size 128
+
