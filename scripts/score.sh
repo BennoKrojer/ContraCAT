e8556075a59be23c40807e3bd147a7266cbda704
krojerb <krojerb@cip.ifi.lmu.de>
2020-04-30 20:55:11 +0200

    more eat_sleep templates

--- scripts/score.sh
+++
@@ -1,3 +0,0 @@
-#python3 -m sockeye.score --target templates/distance/de_bpe --source templates/distance/en_bpe --output outputs/templates/concat22_dist --model models_dario/subtitles/concat-2-2/ --device-ids 0 --output-type score --batch-size 128
-python3 -m sockeye.score --target templates/animals/de_bpe --source templates/animals/en_bpe --output outputs/templates/concat22_pattern_match --model models_dario/subtitles/concat-2-2/ --device-ids 1 --output-type score --batch-size 128
-
--- scripts/templates/create_eat_sleep.py
+++ scripts/templates/create_eat_sleep.py
@@ -4,7 +4,7 @@ from mosestokenizer import MosesTokenizer, MosesPunctuationNormalizer
 nouns = {'m': [], 'f': [], 'n': []}
 nominative = {'m': 'Der', 'f': 'Die', 'n': 'Das'}
 dativ = {'m': 'dem', 'f': 'der', 'n': 'dem'}
-
+specification = 'sleep_eat_eat'
 with open('../../templates/animals/nouns', 'r') as file:
     for line in file:
         en, de, gender = line.split(',')
@@ -22,57 +22,58 @@ for m_en,m_de in nouns['f']:
     for f_en,f_de in nouns['n']:
         pairs.append((('f', m_en, m_de), ('n', f_en, f_de)))
 
-with open('../../templates/animals/de_tok', 'w') as tokenized_de, \
-        open('../../templates/animals/en_tok','w') as tokenized_en,\
+with open(f'../../templates/animals/{specification}/de_tok', 'w') as tokenized_de, \
+        open(f'../../templates/animals/{specification}/en_tok','w') as tokenized_en,\
         MosesPunctuationNormalizer('en') as norm,\
         MosesTokenizer('de') as tok_de,\
         MosesTokenizer('en') as tok_en,\
-        open('../../templates/animals/correct', 'w') as correct:
+        open(f'../../templates/animals/{specification}/correct', 'w') as correct:
 
     for first, second in pairs:
-        en_template1 = ' '.join(tok_en(norm(f'The {first[1]} is eating and the {second[1]} is sleeping. It is eating '
+        en_template1 = ' '.join(tok_en(norm(f'The {first[1]} is sleeping and the {second[1]} is eating. It is eating '
                                             f'a lot.')))
 
-        en_template2 = ' '.join(tok_en(norm(f'The {second[1]} is eating and the {first[1]} is sleeping. It is eating '
+        en_template2 = ' '.join(tok_en(norm(f'The {second[1]} is sleeping and the {first[1]} is eating. It is eating '
                                             f'a lot.')))
+
         for _ in range(3):
             tokenized_en.write(en_template1 + '\n')
-            correct.write(first[0]+'\n')
+            correct.write(second[0]+'\n')
         for _ in range(3):
             tokenized_en.write(en_template2 + '\n')
-            correct.write(second[0] + '\n')
+            correct.write(first[0] + '\n')
 
-        ihn1 = ' '.join(tok_de(norm(f'{nominative[first[0]]} {first[2]} isst und {nominative[second[0]].lower()} '
-                                    f'{second[2]} schläft. Er isst viel.')))
+        ihn1 = ' '.join(tok_de(norm(f'{nominative[first[0]]} {first[2]} schläft und {nominative[second[0]].lower()} '
+                                    f'{second[2]} isst. Er isst viel.')))
 
         tokenized_de.write(ihn1+'\n')
-        sie1 = ' '.join(tok_de(norm(f'{nominative[first[0]]} {first[2]} isst und {nominative[second[0]].lower()} '
-                                    f'{second[2]} schläft. Sie isst viel.')))
+        sie1 = ' '.join(tok_de(norm(f'{nominative[first[0]]} {first[2]} schläft und {nominative[second[0]].lower()} '
+                                    f'{second[2]} isst. Sie isst viel.')))
 
         tokenized_de.write(sie1+'\n')
-        es1 = ' '.join(tok_de(norm(f'{nominative[first[0]]} {first[2]} isst und {nominative[second[0]].lower()} '
-                                    f'{second[2]} schläft. Es isst viel.')))
+        es1 = ' '.join(tok_de(norm(f'{nominative[first[0]]} {first[2]} schläft und {nominative[second[0]].lower()} '
+                                    f'{second[2]} isst. Es isst viel.')))
 
         tokenized_de.write(es1+'\n')
 
-        ihn2 = ' '.join(tok_de(norm(f'{nominative[second[0]]} {second[2]} isst und {nominative[first[0]].lower()} '
-                                    f'{first[2]} schläft. Er isst viel.')))
+        ihn2 = ' '.join(tok_de(norm(f'{nominative[second[0]]} {second[2]} schläft und {nominative[first[0]].lower()} '
+                                    f'{first[2]} isst. Er isst viel.')))
 
         tokenized_de.write(ihn2+'\n')
-        sie2 = ' '.join(tok_de(norm(f'{nominative[second[0]]} {second[2]} isst und {nominative[first[0]].lower()} '
-                                    f'{first[2]} schläft. Sie isst viel.')))
+        sie2 = ' '.join(tok_de(norm(f'{nominative[second[0]]} {second[2]} schläft und {nominative[first[0]].lower()} '
+                                    f'{first[2]} isst. Sie isst viel.')))
 
         tokenized_de.write(sie2+'\n')
 
-        es2 = ' '.join(tok_de(norm(f'{nominative[second[0]]} {second[2]} isst und {nominative[first[0]].lower()} '
-                                    f'{first[2]} schläft. Es isst viel.')))
+        es2 = ' '.join(tok_de(norm(f'{nominative[second[0]]} {second[2]} schläft und {nominative[first[0]].lower()} '
+                                    f'{first[2]} isst. Es isst viel.')))
         tokenized_de.write(es2+'\n')
 
 
-command = 'subword-nmt apply-bpe -c ../../models_dario/subtitles/ende.bpe --glossaries "<SEP>" < ' \
-          '../../templates/animals/en_tok > ../../templates/animals/en_bpe'
+command = f'subword-nmt apply-bpe -c ../../models_dario/subtitles/ende.bpe --glossaries "<SEP>" < ' \
+          f'../../templates/animals/{specification}/en_tok > ../../templates/animals/{specification}/en_bpe'
 os.system(command)
 
-command = 'subword-nmt apply-bpe -c ../../models_dario/subtitles/ende.bpe --glossaries "<SEP>" < ' \
-                  '../../templates/animals/de_tok > ../../templates/animals/de_bpe'
+command = f'subword-nmt apply-bpe -c ../../models_dario/subtitles/ende.bpe --glossaries "<SEP>" < ' \
+                  f'../../templates/animals/{specification}/de_tok > ../../templates/animals/{specification}/de_bpe'
 os.system(command)
