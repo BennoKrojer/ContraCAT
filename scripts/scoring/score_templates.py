e91910de19746de0429ab36cc6e0efec196004e0
Benno Krojer <krojerb@cis.uni-muenchen.de>
2020-05-01 12:37:16 +0200

    scored all eat_drink templates

--- scripts/scoring/score_templates.py
+++ scripts/scoring/score_templates.py
@@ -2,8 +2,12 @@ import os
 
 main_dir = 'templates/animals/'
 for path, _, files in os.walk(main_dir):
-    if files and 'no_object' not in path:
-        command = f'python3 -m sockeye.score --target {path}de_bpe --source {path}en_bpe' \
-                  f' --output outputs/{path}concat22_pattern_match ' \
-                  f'--model models_dario/subtitles/concat-2-2/ --device-ids 1 --output-type score --batch-size 128'
+    if len(files) > 1 and 'no_object' not in path:
+        print(path)
+        output = f'outputs/{path}'
+        os.makedirs(output, exist_ok=True)
+        output = output + '/concat22_pattern_match'
+        command = f'python3 -m sockeye.score --target {path}/de_bpe --source {path}/en_bpe' \
+                  f' --output {output}' \
+                  f' --model models_dario/subtitles/concat-2-2/ --device-ids 1 --output-type score --batch-size 128'
         os.system(command)
