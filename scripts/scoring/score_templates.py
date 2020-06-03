7c46493f6151f1f812506f082b4eeee7437a13e2
krojerb <krojerb@cip.ifi.lmu.de>
2020-06-03 21:35:45 +0200

    wip

--- scripts/scoring/score_templates.py
+++ scripts/scoring/score_templates.py
@@ -4,7 +4,7 @@ main_dir = '../../templates_SEP_fixed/'
 for path, _, files in os.walk(main_dir):
     if len(files) > 1:
         print(path)
-        command = f'python3 -m sockeye.score --target {path}/de_bpe --source {path}/en_bpe' \
-                  f' --output concat22' \
-                  f' --model models_dario/subtitles/concat-2-2/ --device-ids 1 --output-type score --batch-size 128'
-        os.system(command)
+        # command = f'python3 -m sockeye.score --target {path}/de_bpe --source {path}/en_bpe' \
+        #           f' --output concat22' \
+        #           f' --model models_dario/subtitles/concat-2-2/ --device-ids 1 --output-type score --batch-size 128'
+        # os.system(command)
