d1b3c5c9b4135067049df5286f0e44ca1574678a
Benno Krojer <krojerb@cis.uni-muenchen.de>
2020-06-05 17:27:49 +0200

    sc

--- scripts/scoring/score.sh
+++ scripts/scoring/score.sh
@@ -1,1 +1,1 @@
-python3 -m sockeye.score --target templates/animals/animacy/fressen/de_bpe --source templates/animals/animacy/fressen/en_bpe --output outputs/templates/animals/animacy/fressen/concat22 --model models_dario/subtitles/concat-2-2/ --device-ids 0 --output-type score --batch-size 128
+python3 -m sockeye.score --target templates_SEP_fixed/distance/and_de_bpe --source templates_SEP_fixed/distance/and_en_bpe --output templates_SEP_fixed/distance/and_concat22 --model models_dario/subtitles/concat-2-2/ --device-ids 0 --output-type score --batch-size 128
