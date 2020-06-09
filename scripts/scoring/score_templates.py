e7626e900571e7d68171056842885f6edfd2a024
krojerb <krojerb@cip.ifi.lmu.de>
2020-06-09 23:00:05 +0200

    gender templates

--- scripts/scoring/score_templates.py
+++ scripts/scoring/score_templates.py
@@ -2,7 +2,7 @@ import os
 
 main_dir = 'templates_SEP_fixed/'
 for path, _, files in os.walk(main_dir):
-    if len(files) > 1:
+    if len(files) == 5:
         print(path)
         command = f'python3 -m sockeye.score --target {path}/de_bpe --source {path}/en_bpe' \
                   f' --output {path}/concat22' \
