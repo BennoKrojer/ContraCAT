bf21fda31e3dae9538f76fb347d0d5c60412f61c
krojerb <krojerb@cip.ifi.lmu.de>
2020-06-17 09:36:34 +0200

    world temp

---
+++ scripts/templates/create_2_world.py
@@ -0,0 +1,88 @@
+import os
+from mosestokenizer import MosesTokenizer, MosesPunctuationNormalizer
+
+animal_en = ['is hungry', 'is looking around', 'is running around']
+animal_de = ['ist hungrig', 'schaut sich um', 'rennt herum']
+food_en = ['had a sweet taste', 'had a bitter taste', 'had a sour taste', 'was cooked']
+food_de = ['hatte einen süßen Geschmack', 'hatte einen bitteren Geschmack', 'hatte einen sauren Geschmack',
+           'war gekocht']
+building_en = ['is made out of concrete', 'is made out of wood', 'is being renovated']
+building_de = ['ist aus Beton', 'ist aus Holz', 'wird gerade renoviert']
+vehicle_en = ['has a broken tire', 'is fast', 'is parked correctly']
+vehicle_de = ['hat einen kaputten Reifen', 'ist schnell', 'ist richtig geparkt']
+
+
+def combine_nouns(animal_path, food_path):
+    ani_nouns = {'m': [], 'f': [], 'n': []}
+    food_nouns = {'m': [], 'f': [], 'n': []}
+    with open(animal_path, 'r') as ani_file, open(food_path, 'r') as food_file:
+        for line in ani_file:
+            en, de, gender = line.split(',')
+            ani_nouns[gender.strip()].append((en, de))
+        for line in food_file:
+            en, de, gender = line.split(',')
+            food_nouns[gender.strip()].append((en, de))
+    pairs = []
+    for gender1 in ani_nouns:
+        for ani_en, ani_de in ani_nouns[gender1]:
+            for gender2 in food_nouns:
+                if gender1 != gender2:
+                    for food_en, food_de in food_nouns[gender2]:
+                        pairs.append(((gender1, ani_en, ani_de), (gender2, food_en, food_de)))
+
+    return pairs
+
+
+type = 'vehicle_building'
+path = f'../../templates_SEP_fixed/2_coreference_step/world_knowledge/{type}/'
+os.makedirs(path, exist_ok=True)
+pairs = combine_nouns('../../templates_SEP_fixed/entity_sets/vehicles', '../../templates_SEP_fixed/entity_sets/buildings')
+nominative = {'m': 'Der', 'f': 'Die', 'n': 'Das'}
+accusative = {'m': 'den', 'f': 'die', 'n': 'das'}
+dative = {'m': 'dem', 'f': 'der', 'n': 'dem'}
+
+
+with open(path + 'de_tok', 'w') as tokenized_de, \
+        open(path + 'en_tok', 'w') as tokenized_en, \
+        MosesPunctuationNormalizer('en') as norm, \
+        MosesTokenizer('de') as tok_de, \
+        MosesTokenizer('en') as tok_en, \
+        open(f'../../templates_SEP_fixed/2_coreference_step/world_knowledge/{type}/correct', 'w') as correct:
+    for sub, obj in pairs:
+        for attr in vehicle_en:
+            en_template1 = ' '.join(tok_en(norm(f'The {sub[1]} is in front of the {obj[1]}. It {attr}.')))
+            en_template1 = en_template1.replace(' . ', ' . <SEP> ')
+            for _ in range(3):
+                tokenized_en.write(en_template1 + '\n')
+                correct.write(sub[0] + '\n')
+        for attr in building_en:
+            en_template2 = ' '.join(tok_en(norm(f'The {sub[1]} is in front of the {obj[1]}. It {attr}.')))
+            en_template2 = en_template2.replace(' . ', ' . <SEP> ')
+            for _ in range(3):
+                tokenized_en.write(en_template2 + '\n')
+                correct.write(obj[0] + '\n')
+
+        for attr in vehicle_de:
+            for pronoun in ['Er', 'Sie', 'Es']:
+                de_phrase = ' '.join(tok_de(norm(f'{nominative[sub[0]]} {sub[2]} ist vor {dative[obj[0]]} '
+                                                 f'{obj[2]}. {pronoun} {attr}.')))
+                de_phrase = de_phrase.replace(' . ', ' . <SEP> ')
+
+                tokenized_de.write(de_phrase + '\n')
+
+        for attr in building_de:
+            for pronoun in ['Er', 'Sie', 'Es']:
+
+                de_phrase = ' '.join(tok_de(norm(f'{nominative[sub[0]]} {sub[2]} ist vor {dative[obj[0]]} '
+                                                 f'{obj[2]}. {pronoun} {attr}.')))
+                de_phrase = de_phrase.replace(' . ', ' . <SEP> ')
+
+                tokenized_de.write(de_phrase + '\n')
+
+command = f'subword-nmt apply-bpe -c ../../models_dario/subtitles/ende.bpe --glossaries "<SEP>" < ' \
+          f'{path}en_tok > {path}en_bpe'
+os.system(command)
+
+command = f'subword-nmt apply-bpe -c ../../models_dario/subtitles/ende.bpe --glossaries "<SEP>" < ' \
+          f'{path}de_tok > {path}de_bpe'
+os.system(command)
--- scripts/templates/eval_eat_sleep.py
+++ scripts/templates/eval_eat_sleep.py
@@ -1,12 +1,16 @@
 import json
+from collections import defaultdict
+
 import numpy
 import os
 
 gender_order = ['m', 'f', 'n']
-dir = '../../templates_SEP_fixed/1_entity_step/inanimate'
+dir = '../../templates_SEP_fixed/2_coreference_step/'
 for path, _, files in os.walk(dir):
     if 'correct' in files:
         predictions = {'correct': [], 'false': []}
+        words_correct = defaultdict(int)
+        words_incorrect = defaultdict(int)
         distr_correct = {'m':0, 'f':0, 'n':0}
         distr_incorrect = {'m':0, 'f':0, 'n':0}
         correct = open(f'{path}/correct', 'r').readlines()
@@ -22,13 +26,19 @@ for path, _, files in os.walk(dir):
             if correct_gender == pred_gender:
                 predictions['correct'].append(de[i+pred])
                 distr_correct[pred_gender] += 1
+                for word in de[i+pred].split(' '):
+                    words_correct[word] += 1
             else:
                 predictions['false'].append(de[i+pred])
                 distr_incorrect[pred_gender] += 1
+                for word in de[i+pred].split(' '):
+                    words_incorrect[word] += 1
         print(path)
         for key, val in predictions.items():
             print(f'{key}: {len(val)}')
         print(distr_correct)
         print(distr_incorrect)
+        print(sorted(list(words_correct.items())[10:], key=lambda x: x[1], reverse=True))
+        print(sorted(list(words_incorrect.items())[10:], key=lambda x: x[1], reverse=True))
         print('\n')
         json.dump(predictions, open(f'{path}/results.json', 'w'), indent=2)
