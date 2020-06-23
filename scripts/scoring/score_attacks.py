a226cdb81afecc92565fdc4628de8f88bc3134a5
Benno Krojer <krojerb@cis.uni-muenchen.de>
2020-06-23 17:45:43 +0200

    x

--- scripts/scoring/score_attacks.py
+++ scripts/scoring/score_attacks.py
@@ -3,12 +3,13 @@ import os
 include = ['but_1sent', 'true:_no_quote', 'woman', 'best_friend', 'company', 'maria', 'male_de', 'female_de',
            'neutral_de']
 model_path = '/mounts/data/proj/dario/CtxTfNMTOld/models/subtitles/benno/'
-models = ['"augmentation-synonym-mod-low","augmentation-synonym-mod-lower","augmentation-synonym-mod-lowest",'
-          '"augmentation-it-mod-low","augmentation-it-mod-lower","augmentation-it-mod-lowest",'
-          '"augmentation-it-nomod-low","augmentation-it-nomod-lower","augmentation-it-nomod-lowest"']
+models = ["augmentation-synonym-mod-low","augmentation-synonym-mod-lower","augmentation-synonym-mod-lowest",
+          "augmentation-it-mod-low","augmentation-it-mod-lower","augmentation-it-mod-lowest",
+          "augmentation-it-nomod-low","augmentation-it-nomod-lower","augmentation-it-nomod-lowest"]
 
-main_dir = '../../ContraPro_Dario/subtitle_bpe/modified'
+main_dir = 'ContraPro_Dario/subtitle_bpe/modified'
 for path, _, files in os.walk(main_dir):
+    print(path)
     valid = False
     for x in include:
         for f in files:
