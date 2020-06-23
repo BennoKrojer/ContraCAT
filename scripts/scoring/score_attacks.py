9d6f0dc96c7c67c7cbc09c82001f534160e4e5f3
krojerb <krojerb@cip.ifi.lmu.de>
2020-06-23 17:28:25 +0200

    x

--- scripts/scoring/score_attacks.py
+++ scripts/scoring/score_attacks.py
@@ -2,8 +2,10 @@ import os
 
 include = ['but_1sent', 'true:_no_quote', 'woman', 'best_friend', 'company', 'maria', 'male_de', 'female_de',
            'neutral_de']
-
-models = ['']
+model_path = '/mounts/data/proj/dario/CtxTfNMTOld/models/subtitles/benno/'
+models = ['"augmentation-synonym-mod-low","augmentation-synonym-mod-lower","augmentation-synonym-mod-lowest",'
+          '"augmentation-it-mod-low","augmentation-it-mod-lower","augmentation-it-mod-lowest",'
+          '"augmentation-it-nomod-low","augmentation-it-nomod-lower","augmentation-it-nomod-lowest"']
 
 main_dir = '../../ContraPro_Dario/subtitle_bpe/modified'
 for path, _, files in os.walk(main_dir):
@@ -16,5 +18,5 @@ for path, _, files in os.walk(main_dir):
         for model in models:
             command = f'python3 -m sockeye.score --target {path}/de_bpe --source {path}/en_bpe' \
                       f' --output {path}/{model}/concat22' \
-                      f' --model models_dario/subtitles/{model}/ --device-ids 1 --output-type score --batch-size 128'
+                      f' --model  {model_path}/{model}/ --device-ids 1 --output-type score --batch-size 128'
             os.system(command)
