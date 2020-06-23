143a2de464fee33ec1cccc08fec176ae60139d0c
krojerb <krojerb@cip.ifi.lmu.de>
2020-06-23 23:33:39 +0200

    x

--- scripts/scoring/score_attacks.py
+++ scripts/scoring/score_attacks.py
@@ -1,19 +1,18 @@
 import os
 
-include = ['but', 'true:_no_quote', 'woman', 'best_friend', 'company', 'maria', 'male', 'female',
+modifications = ['but', 'true:_no_quote', 'woman', 'best_friend', 'company', 'maria', 'male', 'female',
            'neutral']
 model_path = '/mounts/data/proj/dario/CtxTfNMTOld/models/subtitles/benno/'
-models = ['"augmentation-synonym-mod-low","augmentation-synonym-mod-lower","augmentation-synonym-mod-lowest",'
-          '"augmentation-it-mod-low","augmentation-it-mod-lower","augmentation-it-mod-lowest",'
-          '"augmentation-it-nomod-low","augmentation-it-nomod-lower","augmentation-it-nomod-lowest"']
+models = ["augmentation-synonym-mod-low","augmentation-synonym-mod-lower","augmentation-synonym-mod-lowest",
+          "augmentation-it-mod-low","augmentation-it-mod-lower","augmentation-it-mod-lowest",
+          "augmentation-it-nomod-low","augmentation-it-nomod-lower","augmentation-it-nomod-lowest"]
 
 main_dir = '../../ContraPro_Dario/subtitle_bpe/modified'
-for mod in include:
-    for path, _, files in os.walk(main_dir):
-        if mod in path:
-            for model in models:
-                command = f'python3 -m sockeye.score --target {path}/de_bpe --source {path}/en_bpe' \
-                          f' --output {path}/{model}/concat22' \
-                          f' --model  {model_path}/{model}/ --device-ids 1 --output-type score --batch-size 128'
-                print(command)
-                # os.system(command)
+for path, _, files in os.walk(main_dir):
+    if path.split('/')[-1] in modifications:
+        for model in models:
+            command = f'python3 -m sockeye.score --target {path}/de_bpe --source {path}/en_bpe' \
+                      f' --output {path}/{model}/scores' \
+                      f' --model  {model_path}/{model}/ --device-ids 1 --output-type score --batch-size 128'
+            print(command)
+            # os.system(command)
