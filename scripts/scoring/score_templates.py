ff03d5df30656fd22179c0731c273b7c06269f80
krojerb <krojerb@cip.ifi.lmu.de>
2020-07-01 00:14:45 +0200

    x

--- scripts/scoring/score_templates.py
+++ scripts/scoring/score_templates.py
@@ -1,6 +1,6 @@
 import os
 
-main_dir = 'templates/0_priors/role_variant'
+main_dir = 'templates/final/baseline_sentence_level'
 models = {'baseline': 'models_dario/subtitles/baseline/baseline/'}
 for model, model_path in models.items():
     for path, _, files in os.walk(main_dir):
